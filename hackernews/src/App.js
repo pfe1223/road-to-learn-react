fcbf044de7e5ab234f99f33987d43a42474a5383
pfe1223 <patrick.ester@gmail.com>
2018-10-23 09:34:00 -0700

    Start JSX
    Start the intro to JSX. Had to quit because of 'npm start' errors on work machine.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+} ];
+
 class App extends Component {
   render() {
   	const helloWorld = 'Welcome to the Road to Learn React';
     return (
       <div className="App">
-      	<h2>{helloWorld}</h2>
+      	{list.map(function(item) {
+          return <div>{item.title}</div>
+        })}
       </div>
     );
   }
