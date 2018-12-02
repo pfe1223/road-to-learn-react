92c0e4e199b7b7da3070a665e38bbcfe5e25d5f2
Patrick <patrick.ester@gmail.com>
2018-12-02 15:30:46 -0800

    Page 25
    Work up through page 25.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 const list = [
+<<<<<<< HEAD
 	{
 		title: 'React',
 		url: 'https://reactjs.org',
@@ -19,12 +20,30 @@ const list = [
 		objectID: 1,
 	},
 ];
+=======
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+} ];
+>>>>>>> fcbf044de7e5ab234f99f33987d43a42474a5383
 
 class App extends Component {
   render() {
   	const helloWorld = 'Welcome to the Road to Learn React';
     return (
       <div className="App">
+<<<<<<< HEAD
       	{list.map(item => 
       		<div key={item.objectID}>
       			<span>
@@ -35,6 +54,11 @@ class App extends Component {
       			<span>{item.points}</span>
       		</div>
       	)}
+=======
+      	{list.map(function(item) {
+          return <div>{item.title}</div>
+        })}
+>>>>>>> fcbf044de7e5ab234f99f33987d43a42474a5383
       </div>
     );
   }
