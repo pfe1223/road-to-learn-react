658d8e7a46fe1ade33f5745f954d8c07541be214
Patrick <patrick.ester@gmail.com>
2018-12-18 18:43:44 -0800

    Split Up Components
    Split up the main logic into their own components.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -49,36 +49,64 @@ class App extends Component {
 	}
 	
   render() {
+  	const {searchTerm, list} = this.state
     return (
-    	const {searchTerm, list} = this.state
       <div className="App">
-      	<form>
-      		<input 
-      			type="text"
-      			onChange={this.onSearchChange} 
-      		/>
-      	</form>
-      	{list.filter(isSearched(searchTerm)).map(item => 
-      		<div key={item.objectID}>
-      			<span>
-      				<a href={item.url}>{item.title}</a>
-      			</span>
-      			<span>{item.author}</span>
-      			<span>{item.num_comments}</span>
-      			<span>{item.points}</span>
-      			<span>
-      				<button
-      					onClick={() => this.onDismiss(item.objectID)}
-      					type='button'
-      				>
-      					Dismiss
-      				</button>
-      			</span>
-      		</div>
-      	)}
+      	<Search
+      	  value = {searchTerm}
+      	  onChange = {this.onSearchChange}
+      	/>
+      	<Table
+      	  list = {list}
+      	  pattern = {searchTerm}
+      	  onDismiss = {this.onDismiss}
+      	/>
       </div>
     );
   }
 }
 
 export default App;
+
+class Search extends Component {
+  render() {
+    const {value, onChange} = this.props
+    return (
+     <form>
+	 	<input 
+       		type="text"
+       		value={value}
+       		onChange={onChange} 
+     	/>
+     </form>
+    )
+  }
+}
+
+class Table extends Component {
+	render() {
+		const {list, pattern, onDismiss} = this.props
+		return (
+			<div>
+				{list.filter(isSearched(pattern)).map(item => 
+      				<div key={item.objectID}>
+      					<span>
+      						<a href={item.url}>{item.title}</a>
+      					</span>
+      					<span>{item.author}</span>
+      					<span>{item.num_comments}</span>
+      					<span>{item.points}</span>
+      					<span>
+      						<button
+      							onClick={() => onDismiss(item.objectID)}
+      							type='button'
+      						>
+      							Dismiss
+      						</button>
+      					</span>
+      				</div>
+      			)}
+			</div>
+		)
+	}
+}
