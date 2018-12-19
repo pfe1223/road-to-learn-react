1eb6ace766bbae01b03d558a21e14c43cc9f3a77
Patrick <patrick.ester@gmail.com>
2018-12-18 20:46:55 -0800

    Composable Components
    Use the "children" prop.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -55,7 +55,9 @@ class App extends Component {
       	<Search
       	  value = {searchTerm}
       	  onChange = {this.onSearchChange}
-      	/>
+      	>
+      		Search
+      	</Search>
       	<Table
       	  list = {list}
       	  pattern = {searchTerm}
@@ -70,10 +72,10 @@ export default App;
 
 class Search extends Component {
   render() {
-    const {value, onChange} = this.props
+    const {value, onChange, children} = this.props
     return (
      <form>
-	 	<input 
+	 	{children} <input 
        		type="text"
        		value={value}
        		onChange={onChange} 
