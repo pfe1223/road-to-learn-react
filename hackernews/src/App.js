10a10be4445fc96e31108a196598faae449c32ed
Patrick <patrick.ester@gmail.com>
2018-12-26 19:16:10 -0800

    Stateless Components
    Refact Table and Button so they are stateless components.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -51,13 +51,15 @@ class App extends Component {
   render() {
   	const {searchTerm, list} = this.state
     return (
-      <div className="App">
-      	<Search
-      	  value = {searchTerm}
-      	  onChange = {this.onSearchChange}
-      	>
+      <div className="page">
+      	<div className="interactions">
+      		<Search
+      	  		value = {searchTerm}
+      	  		onChange = {this.onSearchChange}
+      		>
       		Search
-      	</Search>
+      		</Search>
+      	</div>
       	<Table
       	  list = {list}
       	  pattern = {searchTerm}
@@ -79,50 +81,33 @@ const Search = ({value, onChange, children}) =>
      	/>
      </form>
 
-class Table extends Component {
-	render() {
-		const {list, pattern, onDismiss} = this.props
-		return (
-			<div>
-				{list.filter(isSearched(pattern)).map(item => 
-      				<div key={item.objectID}>
-      					<span>
-      						<a href={item.url}>{item.title}</a>
-      					</span>
-      					<span>{item.author}</span>
-      					<span>{item.num_comments}</span>
-      					<span>{item.points}</span>
-      					<span>
-      						<Button
-      							onClick={() => onDismiss(item.objectID)}
-      							type='button'
-      						>
-      							Dismiss
-      						</Button>
-      					</span>
-      				</div>
-      			)}
-			</div>
-		)
-	}
-}
+const Table = ({list, pattern, onDismiss}) =>
+	<div>
+		{list.filter(isSearched(pattern)).map(item => 
+      		<div key={item.objectID}>
+      			<span>
+      				<a href={item.url}>{item.title}</a>
+      			</span>
+      			<span>{item.author}</span>
+      			<span>{item.num_comments}</span>
+      			<span>{item.points}</span>
+      			<span>
+      				<Button
+      					onClick={() => onDismiss(item.objectID)}
+      					type='button'
+      				>
+      					Dismiss
+      				</Button>
+      			</span>
+      		</div>
+      	)}
+	</div>	
 
-class Button extends Component {
-	render() {
-		const {
-			onClick,
-			className = '',
-			children
-		} = this.props
-		
-		return (
-			<button
-				onClick = {onClick}
-				className = {className}
-				type="button"
-			>
-				{children}
-			</button>
-		)
-	}
-}
+const Button = ({onClick, className = '', children}) =>
+	<button
+		onClick = {onClick}
+		className = {className}
+		type="button"
+	>
+		{children}
+	</button>
